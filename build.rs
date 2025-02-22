a0cc3de610d897baf8a730e197c3af8ac61ec496
Rodrigo Duran <rads2995@gmail.com>
2025-02-22 12:59:36 -0500

    Possible better way of building third-party libraries only when changed.

--- build.rs
+++ build.rs
@@ -1,38 +1,42 @@
 use cmake::Config;
-use std::env;
 use std::path::{Path, PathBuf};
 
 fn main() {
-    let build_libs: bool = env::var("BUILD_LIBS")
-        .map(|v: String| v == "1")
-        .unwrap_or(false);
+    let whispercpp_path: &Path = Path::new("./third-party/whisper.cpp");
+    let llamacpp_path: &Path = Path::new("./third-party/llama.cpp");
 
-    if build_libs {
-        let whispercpp_path: &Path = Path::new("./third-party/whisper.cpp");
-        let llamacpp_path: &Path = Path::new("./third-party/llama.cpp");
+    println!("cargo:rerun-if-changed={}", whispercpp_path.display());
+    println!("cargo:rerun-if-changed={}", llamacpp_path.display());
+    println!(
+        "cargo:rerun-if-changed={}",
+        whispercpp_path.join("CMakeLists.txt").display()
+    );
+    println!(
+        "cargo:rerun-if-changed={}",
+        llamacpp_path.join("CMakeLists.txt").display()
+    );
 
-        let whispercpp_cmake: PathBuf = Config::new(whispercpp_path)
-            .define("GGML_VULKAN", "1")
-            .define("BUILD_SHARED_LIBS", "ON")
-            .build_arg("-j6")
-            .build();
+    let whispercpp_cmake: PathBuf = Config::new(whispercpp_path)
+        .define("GGML_VULKAN", "1")
+        .define("BUILD_SHARED_LIBS", "ON")
+        .build_arg("-j6")
+        .build();
 
-        let llamacpp_cmake: PathBuf = Config::new(llamacpp_path)
-            .define("GGML_VULKAN", "1")
-            .define("BUILD_SHARED_LIBS", "ON")
-            .build_arg("-j6")
-            .build();
+    let llamacpp_cmake: PathBuf = Config::new(llamacpp_path)
+        .define("GGML_VULKAN", "1")
+        .define("BUILD_SHARED_LIBS", "ON")
+        .build_arg("-j6")
+        .build();
 
-        println!(
-            "cargo:rustc-link-search=native={}",
-            whispercpp_cmake.join("lib").display()
-        );
-        println!("cargo:rustc-link-lib=dylib=whisper");
+    println!(
+        "cargo:rustc-link-search=native={}",
+        whispercpp_cmake.join("lib").display()
+    );
+    println!("cargo:rustc-link-lib=dylib=whisper");
 
-        println!(
-            "cargo:rustc-link-search=native={}",
-            llamacpp_cmake.join("lib").display()
-        );
-        println!("cargo:rustc-link-lib=dylib=llama");
-    }
+    println!(
+        "cargo:rustc-link-search=native={}",
+        llamacpp_cmake.join("lib").display()
+    );
+    println!("cargo:rustc-link-lib=dylib=llama");
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,13 @@ mod bindings {
 }
 
 use bindings::whisper::{
-    whisper_context_default_params, 
-    whisper_context_params, 
+    whisper_context, whisper_context_default_params, whisper_context_params,
     whisper_init_from_file_with_params,
-    whisper_context,
 };
 
 fn main() {
-    
-    let mut cparams: whisper_context_params = unsafe { whisper_context_default_params() };
 
-    let ctx: *mut whisper_context = unsafe {whisper_init_from_file_with_params(path_model, cparams)};
+    // let mut cparams: whisper_context_params = unsafe { whisper_context_default_params() };
 
+    // let ctx: *mut whisper_context = unsafe {whisper_init_from_file_with_params(path_model, cparams)};
 }
