a2432cee05b00eac97c50b09ef13fd7e7148ec4f
Rodrigo Duran <rads2995@gmail.com>
2025-02-24 22:14:45 -0500

    Figured out how to avoid unecessary rebuilds of the library.

--- build.rs
+++ build.rs
@@ -1,42 +1,39 @@
 use cmake::Config;
-use std::path::{Path, PathBuf};
+use std::fs;
+use std::path::PathBuf;
 
 fn main() {
-    let whispercpp_path: &Path = Path::new("./third-party/whisper.cpp");
-    let llamacpp_path: &Path = Path::new("./third-party/llama.cpp");
+    let whispercpp_path: PathBuf = fs::canonicalize("./third-party/whisper.cpp").unwrap();
+    let llamacpp_path: PathBuf = fs::canonicalize("./third-party/llama.cpp").unwrap();
 
     println!("cargo:rerun-if-changed={}", whispercpp_path.display());
     println!("cargo:rerun-if-changed={}", llamacpp_path.display());
-    println!(
-        "cargo:rerun-if-changed={}",
-        whispercpp_path.join("CMakeLists.txt").display()
-    );
-    println!(
-        "cargo:rerun-if-changed={}",
-        llamacpp_path.join("CMakeLists.txt").display()
-    );
 
-    let whispercpp_cmake: PathBuf = Config::new(whispercpp_path)
+    let whispercpp_cmake: PathBuf = Config::new(&whispercpp_path)
         .define("GGML_VULKAN", "1")
         .define("BUILD_SHARED_LIBS", "ON")
-        .build_arg("-j6")
+        .generator("Ninja")
+        .build_target("all")
+        .out_dir(&whispercpp_path)
         .build();
 
-    let llamacpp_cmake: PathBuf = Config::new(llamacpp_path)
+    let llamacpp_cmake: PathBuf = Config::new(&llamacpp_path)
         .define("GGML_VULKAN", "1")
         .define("BUILD_SHARED_LIBS", "ON")
-        .build_arg("-j6")
+        .generator("Ninja")
+        .build_target("all")
+        .out_dir(&llamacpp_path)
         .build();
 
     println!(
         "cargo:rustc-link-search=native={}",
-        whispercpp_cmake.join("lib").display()
+        whispercpp_cmake.join("build/src").display()
     );
     println!("cargo:rustc-link-lib=dylib=whisper");
 
     println!(
         "cargo:rustc-link-search=native={}",
-        llamacpp_cmake.join("lib").display()
+        llamacpp_cmake.join("build/bin").display()
     );
     println!("cargo:rustc-link-lib=dylib=llama");
 }
